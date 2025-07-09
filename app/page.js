b61852029d7c29a254d8a0d0805ed40822cda8e9
Faiq <faiqhassan670@gmail.com>
2025-07-10 01:53:01 +0200

    make progress

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>faiq</div>
+    <div><br></br>
+    
+      
+    </div>  
+      
       );
 }
