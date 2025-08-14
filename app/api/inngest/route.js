c47af508dd1b3838937456c21ae912638e2e2a44
Faiq <faiqhassan670@gmail.com>
2025-08-14 15:28:58 +0200

    ready to deploy

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,6 +5,7 @@ import { serve } from "inngest/next";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   functions: [
     /* your functions will be passed here later! */
     generateIndustryInsights,
