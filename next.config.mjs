dd5842679b363e03d51c5ef75febed8fbf719f1e
Faiq <faiqhassan670@gmail.com>
2025-08-14 13:59:30 +0200

    ready to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
