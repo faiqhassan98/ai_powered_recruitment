0e564748f0b1e731aef4862dbe7053e517cd20f1
Faiq <faiqhassan670@gmail.com>
2025-08-14 14:35:27 +0200

    ready to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      html2canvas: path.resolve(
-        __dirname,
-        "node_modules/html2canvas-pro"
-      ),
+      html2canvas: path.resolve(__dirname, "node_modules/html2canvas-pro"),
     };
     return config;
   },
@@ -29,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
